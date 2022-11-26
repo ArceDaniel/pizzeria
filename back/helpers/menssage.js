8698ec382cba07358d05c2f6696f32bb08230b0b
ArceDaniel <jonathandanielarce9@gmail.com>
2022-11-26 14:14:59 -0300

    e

--- back/helpers/menssage.js
+++ back/helpers/menssage.js
@@ -5,6 +5,6 @@ const sendMenssage = (obj)=>{
 
 
 
-    return `https://api.whatsapp.com/send?phone=${process.env.PHONE}&text=mi%20pedido%20es:%20http://localhost:3000/view/${obj.number}`
+    return `https://api.whatsapp.com/send?phone=${process.env.PHONE}&text=mi%20pedido%20es:%20${process.env.URL_FRONT}/view/${obj.number}`
 }
 export default sendMenssage
